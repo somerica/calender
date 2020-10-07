e011a1c55a2a1459b7f3387e035233bb177f763d
Somer Jackson <somer.jackson@gmail.com>
2020-10-07 10:38:04 -0400

    added array for hours correlating to color

--- script.js
+++ script.js
@@ -17,6 +17,8 @@ $(document).ready(function() {
     var days=moment().add(3, 'days').calendar();
     var NowMoment = moment();
     
+    
+    
       
     var hours=[
     "9am",
@@ -29,6 +31,34 @@ $(document).ready(function() {
     "4pm",
     "5pm",
     ];   
+
+const rows = document.getElementsByClassName("row");
+let currentHour = parseInt(moment().format('H'));
+
+    Array.from(rows).forEach(row => {
+      let
+        rowIdString = row.id,
+        rowHour;
+      if (rowIdString) {
+        rowHour = parseInt(rowIdString);
+      }
+      if (rowHour) {
+        
+        if (currentHour === rowHour) {
+          setColor(row, "red");
+        } else if ((currentHour < rowHour) && (currentHour > rowHour - 6)) {
+          setColor(row, "green");
+        } else if ((currentHour > rowHour) && (currentHour < rowHour + 6)) {
+          setColor(row, "lightgrey");
+        } else {
+          setColor(row, "white");
+        }
+      }
+    });
+
+    function setColor(element, color) {
+  element.style.backgroundColor = color;
+    }
 
     var dayAndTime=document.getElementById("dayAndTime")
 
